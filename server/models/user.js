9cbc7d2cf0cbfa18ca84ab1aeb52478a65992792
yosuke tangi <yosuke1024@yosukenoMacBook-Pro.local>
2021-08-10 14:19:26 +0900

    edit models

--- server/models/user.js
+++ server/models/user.js
@@ -1,27 +1,15 @@
 'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class user extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  user.init({
+  const user = sequelize.define('user', {
     name: DataTypes.STRING,
-    password_hash: DataTypes.STRING,
     img: DataTypes.STRING,
+    password_hash: DataTypes.STRING,
     status_message: DataTypes.STRING
   }, {
-    sequelize,
-    modelName: 'user',
     underscored: true,
   });
+  user.associate = function(models) {
+    user.hasMany(models.roomsuser, { foreignKey: 'user_id' });
+  };
   return user;
 };
