4408e1097b59bb2cf0b2cf03cc0844c2b077c5b8
yosuke tangi <yosuke1024@yosukes-MacBook-Pro.local>
2021-08-29 12:35:37 +0900

    add find-room-id function

---
+++ server/src/api/room.js
@@ -0,0 +1,37 @@
+const Message = require("../../models").message;
+ const Room = require("../../models").room;
+ const Roomuser = require("../../models").roomsuser;
+
+ const findRoomId = async (req, res) => {
+    const currentUserId = req.param("id");
+    const { friendId } = req.body;
+    try {
+      const rooms = await Room.findAll({
+        include: [{ model: Roomuser }],
+      });
+      let currentRoomId = 0;
+      rooms.map((room) => {
+        if (
+          Number(currentUserId) == room.dataValues.roomsusers[0].user_id &&
+          friendId == room.dataValues.roomsusers[1].user_id
+        ) {
+          currentRoomId = room.id;
+          return;
+        }
+        if (
+          Number(currentUserId) == room.dataValues.roomsusers[1].user_id &&
+          friendId == room.dataValues.roomsusers[0].user_id
+        ) {
+          currentRoomId = room.id;
+        }
+        return;
+      });
+      res.status(200).json({ currentRoomId });
+    } catch (error) {
+      res.status(500).json({ error: "サーバーエラー" });
+    }
+  };
+
+  module.exports = {
+    findRoomId,
+ };
