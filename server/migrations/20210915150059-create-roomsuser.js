2bc45f1334fe2e3cbc4c46ccefb6a6d05605c7c7
Hatchan800 <pplatinumoon@gmail.com>
2021-09-17 18:27:28 +0900

    edit migrations

--- server/migrations/20210915150059-create-roomsuser.js
+++ server/migrations/20210915150059-create-roomsuser.js
@@ -1,30 +1,38 @@
-'use strict';
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('roomsusers', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      user_id: {
-        type: Sequelize.INTEGER
-      },
-      room_id: {
-        type: Sequelize.INTEGER
-      },
-      created_at: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updated_at: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('roomsusers');
-  }
-};
+  'use strict';
+  module.exports = {
+    up: (queryInterface, Sequelize) => {
+      return queryInterface.createTable('roomsusers', {
+        id: {
+          allowNull: false,
+          autoIncrement: true,
+          primaryKey: true,
+          type: Sequelize.INTEGER
+        },
+        user_id: {
+          type: Sequelize.INTEGER,
+          references: {
+            model:'users',
+            key: 'id'
+          }
+        },
+        room_id: {
+          type: Sequelize.INTEGER,
+          references: {
+            model: 'rooms',
+            key: 'id'
+          }
+        },
+        created_at: {
+          allowNull: false,
+          type: Sequelize.DATE
+        },
+        updated_at: {
+          allowNull: false,
+          type: Sequelize.DATE
+        }
+      });
+    },
+    down: (queryInterface, Sequelize) => {
+      return queryInterface.dropTable('roomsusers');
+    }
+  };
