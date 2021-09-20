2c99d890bee067f5ea58b0fc8f0820ed976bd573
Hatchan800 <pplatinumoon@gmail.com>
2021-09-20 15:39:18 +0900

    complete api routings

--- server/src/index.js
+++ server/src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
-const apiServer = app.listen(3000, () => { console.log("PORT:" + apiServer.address().port) });
+const bodyParser = require("body-parser");
+const apiServer = app.listen(3000, function () { 
+  console.log("PORT:" + apiServer.address().port); 
+});
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
@@ -15,15 +18,23 @@ app.get("/", (req, res) => {
     ], 
   });
 });
+const { logIn, setProfile, getFriends } = require("./api/users");
 app.post("/api/users/session", (req, res) => {
+    logIn(req, res);
 });
 app.post("/api/users/:id/edit", (req, res) => {
+    setProfile(req, res);
 });
 app.post("/api/users/:id/friends", (req, res) => {
+    getFriends(req, res);
 });
+const { findRoomId, getMessages, sendMessage } = require("./api/rooms");
 app.post("/api/users/:id/find_room_id", (req, res) => {
+    findRoomId(req, res);
 });
 app.get("/api/rooms/:id/messages", (req, res) => {
+    getMessages(req, res);
 });
 app.post("/api/rooms/:id/message", (req, res) => {
+    sendMessage(req, res);
 });
