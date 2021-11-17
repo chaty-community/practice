0709516a2d79327d178efbf4dfed2ef91b0512ab
Hatchan800 <pplatinumoon@gmail.com>
2021-11-17 22:15:39 +0900

    Users.jsの作成

--- mobile/App.js
+++ mobile/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Profile from './src/pages/Profile';
+import Users from './src/pages/Users';
 console.disableYellowBox = true;
 
 export default function App() {
-  return <Profile />;
+  return <Users />;
 }
