56dcb0aa214f5e72caa7fc95caed7542635c15bd
Hatchan800 <pplatinumoon@gmail.com>
2021-11-22 11:56:05 +0900

    add get-friends function

--- server/src/api/users.js
+++ server/src/api/users.js
@@ -72,7 +72,24 @@ const setProfile = async (req, res) => {
   }
 };
 
+const getFriends = async (req, res) => {
+  const user_id = req.param("id");
+  try {
+    const users = await User.findAll({
+      where: {
+        id: {
+          [Op.not]: user_id,
+        },
+      },
+    });
+    res.status(200).json({ friends: users });
+  } catch (error) {
+    res.status(500).json({ error: "サーバーエラー" });
+  }
+};
+
 module.exports = {
   logIn,
   setProfile,
+  getFriends,
 };
