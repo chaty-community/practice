d117fc152fda8a4d2f4898a5a13b1bbf11332aef
Hatchan800 <pplatinumoon@gmail.com>
2021-11-28 17:42:10 +0900

    connect socket at App.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import Navigator from './src/pages/Navigator';
 console.disableYellowBox = true;
+import urls from "./src/env";
+import io from "socket.io-client";
+export const socket = io.connect(urls.socket_server);
 
 export default function App() {
   return <Navigator />;
